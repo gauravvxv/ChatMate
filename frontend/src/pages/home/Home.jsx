c928438ba4ba357595e06a78492be094894d7a33
Gaurav Sharma <gauraxvx0410@gmail.com>
2024-10-02 13:16:57 +0530

    aa

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,15 +3,17 @@ import Sidebar from '../../components/sidebar/Sidebar'
 import MessageContainer from '../../components/messages/MessageContainer'
 const Home = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 
-    bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
+    <div className='flex sm:h-[450px] md:h-[550px] h-screen rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 
-      <Sidebar />
+  <div className='overflow-y-auto'>
+    <Sidebar />
+  </div>
 
-      <MessageContainer />
+  <div className=' overflow-y-auto'>
+    <MessageContainer />
+  </div>
 
-
-    </div>
+</div>
   )
 }
 
